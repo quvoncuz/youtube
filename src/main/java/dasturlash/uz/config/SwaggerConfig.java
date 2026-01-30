417a0a9d5ac40e78d991e3976ad2424bd2aa9886
quvoncuz <quvoncbe.uz@gmail.com>
2026-01-30 15:32:20 +0500

    Swagger configuration

---
+++ src/main/java/dasturlash/uz/config/SwaggerConfig.java
@@ -0,0 +1,74 @@
+package dasturlash.uz.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // general info
+        Info info = new Info()
+                .title("Youtube API-lar")
+                .version("1.0.0")
+                .description("Quyida Youtube loyihasi uchun API hujjatlar tagdim qilingan.")
+                .contact(new Contact()
+                        .name("Quvonchbek")
+                        .email("quvoncbe.uz@gmail.com")
+                        .url("https://t.me/quvoncuz")
+                )
+                .license(new License()
+                        .name("quvonc.uz")
+                        .url("https://quvonc.uz/")
+                )
+                .termsOfService("Savol javob guruhi: https://t.me/code_uz_group");
+
+        // servers (ishlatiladigan serverlar)
+        Server server1 = new Server()
+                .description("Local")
+                .url("http://localhost:8080");
+
+        Server server2 = new Server()
+                .description("DEV")
+                .url("http://api.mazgi.uz");
+
+        Server server3 = new Server()
+                .description("PROD")
+                .url("http://api.giybat.uz");
+
+        // security type (bizning holatda JWT)
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("bearerAuth");
+
+        SecurityScheme securityScheme = new SecurityScheme();
+        securityScheme.setName("bearerAuth");
+        securityScheme.setType(SecurityScheme.Type.HTTP);
+        securityScheme.bearerFormat("JWT");
+        securityScheme.setIn(SecurityScheme.In.HEADER);
+        securityScheme.setScheme("bearer");
+
+        Components components = new Components();
+        components.addSecuritySchemes("bearerAuth", securityScheme);
+
+        // collect all together
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.setInfo(info);
+        openAPI.setServers(List.of(server1, server2, server3));
+        openAPI.setSecurity(List.of(securityRequirement));
+        openAPI.components(components);
+
+        // return-xe
+        return openAPI;
+    }
+}
